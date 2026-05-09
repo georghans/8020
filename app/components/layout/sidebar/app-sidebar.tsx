a8954c250de3d0a44285adcc7c0a052cd4442b7f
Georg Hans <georg.hans@rio.cloud>
2026-05-09 20:03:29 +0200

    clean up sidebar

--- app/components/layout/sidebar/app-sidebar.tsx
+++ app/components/layout/sidebar/app-sidebar.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { groupChatsByDate } from "@/app/components/history/utils"
 import { useBreakpoint } from "@/app/hooks/use-breakpoint"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import {
@@ -10,32 +9,18 @@ import {
   SidebarHeader,
   useSidebar,
 } from "@/components/ui/sidebar"
-import { useChats } from "@/lib/chat-store/chats/provider"
 import {
-  ChatTeardropText,
+  ClockCounterClockwise,
   MagnifyingGlass,
   NotePencilIcon,
   X,
 } from "@phosphor-icons/react"
-import { Pin } from "lucide-react"
-import { useParams, useRouter } from "next/navigation"
-import { useMemo } from "react"
+import { useRouter } from "next/navigation"
 import { HistoryTrigger } from "../../history/history-trigger"
-import { SidebarList } from "./sidebar-list"
-import { SidebarProject } from "./sidebar-project"
 
 export function AppSidebar() {
   const isMobile = useBreakpoint(768)
   const { setOpenMobile } = useSidebar()
-  const { chats, pinnedChats, isLoading } = useChats()
-  const params = useParams<{ chatId: string }>()
-  const currentChatId = params.chatId
-
-  const groupedChats = useMemo(() => {
-    const result = groupChatsByDate(chats, "")
-    return result
-  }, [chats])
-  const hasChats = chats.length > 0
   const router = useRouter()
 
   return (
@@ -77,12 +62,12 @@ export function AppSidebar() {
             </button>
             <HistoryTrigger
               hasSidebar={false}
-              classNameTrigger="bg-transparent hover:bg-accent/80 hover:text-foreground text-primary relative inline-flex w-full items-center rounded-md px-2 py-2 text-sm transition-colors group/search"
-              icon={<MagnifyingGlass size={24} className="mr-2" />}
+              classNameTrigger="bg-transparent hover:bg-accent/80 hover:text-foreground text-primary relative inline-flex w-full items-center rounded-md px-2 py-2 text-sm transition-colors group/history"
+              icon={<ClockCounterClockwise size={20} className="mr-2" />}
               label={
                 <div className="flex w-full items-center gap-2">
-                  <span>Search</span>
-                  <div className="text-muted-foreground ml-auto text-xs opacity-0 duration-150 group-hover/search:opacity-100">
+                  <span>Chat History</span>
+                  <div className="text-muted-foreground ml-auto text-xs opacity-0 duration-150 group-hover/history:opacity-100">
                     ⌘+K
                   </div>
                 </div>
@@ -90,43 +75,6 @@ export function AppSidebar() {
               hasPopover={false}
             />
           </div>
-          <SidebarProject />
-          {isLoading ? (
-            <div className="h-full" />
-          ) : hasChats ? (
-            <div className="space-y-5">
-              {pinnedChats.length > 0 && (
-                <div className="space-y-5">
-                  <SidebarList
-                    key="pinned"
-                    title="Pinned"
-                    icon={<Pin className="size-3" />}
-                    items={pinnedChats}
-                    currentChatId={currentChatId}
-                  />
-                </div>
-              )}
-              {groupedChats?.map((group) => (
-                <SidebarList
-                  key={group.name}
-                  title={group.name}
-                  items={group.chats}
-                  currentChatId={currentChatId}
-                />
-              ))}
-            </div>
-          ) : (
-            <div className="flex h-[calc(100vh-160px)] flex-col items-center justify-center">
-              <ChatTeardropText
-                size={24}
-                className="text-muted-foreground mb-1 opacity-40"
-              />
-              <div className="text-muted-foreground text-center">
-                <p className="mb-1 text-base font-medium">No chats yet</p>
-                <p className="text-sm opacity-70">Start a new conversation</p>
-              </div>
-            </div>
-          )}
         </ScrollArea>
       </SidebarContent>
       <SidebarFooter className="border-border/40 mb-2 border-t p-3" />
